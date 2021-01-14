6e1cbcbf97c9629788d3f74158c4728e22aa74ba
Tarun Kumar Sukhu <tksukhu@gmail.com>
2021-01-14 10:46:37 +0530

    added next app

--- cypress/integration/basic.spec.ts
+++ cypress/integration/basic.spec.ts
@@ -2,7 +2,7 @@
 
 describe('Smoke Test', () => {
   it('should render', () => {
-    cy.visit('http://localhost:1234');
+    cy.visit('http://localhost:3000');
 
     cy.get('[data-test-id="zop"]').contains('hello-worldzz');
   });
